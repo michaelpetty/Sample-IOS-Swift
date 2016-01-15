8b7574660c551cefc8340ffc9f789a17c945ef4c
michaelpetty <michael.petty@gmail.com>
2016-01-14 23:57:06 -0800

    change to TableView Controller for ContactsViewController; add fetchRequest for Contacts; add tableView details

--- SampleIOSSwift/ContactsViewControlller.swift
+++ SampleIOSSwift/ContactsViewControlller.swift
@@ -14,21 +14,67 @@ class ContactsViewController: UITableViewController {
 
     //set to access CNContactStore, Contacts
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    var contacts = [CNContact]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         appDelegate.requestForAccess() { (accessGranted) -> Void in
             if accessGranted {
-                print("Authorized for Contacts")
-            }
-    
-        }
+                let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey]
+                let fetchRequest = CNContactFetchRequest(keysToFetch: keys)
+                //var contacts = [CNContact]()
+                var message: String!
+                
+                let contactStore = self.appDelegate.contactStore
+                do {
+                    try contactStore.enumerateContactsWithFetchRequest(fetchRequest, usingBlock: { (let contact, let stop) -> Void in
+                        self.contacts.append(contact)
+                    })
+                    
+                    if self.contacts.count == 0 {
+                        message = "No contacts were found"
+                    }
+                }
+                catch {
+                    message = "Unable to fetch contacts"
+                }
+                
+                if message != nil {
+                    dispatch_async(dispatch_get_main_queue(), { ()  -> Void in
+                        self.appDelegate.showMessage(message)
+                        
+                    })
+                } else {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.tableView!.reloadData()
+                    })
+                }
+                
+            }}
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // Table View
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contacts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let contact = contacts[indexPath.row] as CNContact
+        cell.textLabel!.text = "\(contact.givenName) \(contact.familyName)"
+        return cell
     }
 
     
